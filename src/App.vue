f0683e22943bb47981aa57844b1562c03c147732
Feralbot <staaasyyaaan@gmail.com>
2023-04-05 15:46:01 +0300

    added loader screen

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
 import headerComponent from "./components/header.vue";
+import loader from "./components/loader.vue";
+import { usePageScrollStore } from "./stores/pageScrollStore";
+const pageScrollStore = usePageScrollStore();
 </script>
 
 <template>
   <headerComponent />
   <router-view />
-  <headerComponent />
+  <loader
+    v-if="pageScrollStore.loading"
+    class="absolute h-screen top-0 opacity-50 w-screen"
+  />
 </template>
